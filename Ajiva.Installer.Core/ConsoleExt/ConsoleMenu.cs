b504637c60a555166c416e975fca2eeb61516be2
xuri02 <t0bl4s.durst@gmail.com>
2021-02-14 22:46:04 +0100

    copied some console assistants from Ajiva Render engine

---
+++ Ajiva.Installer.Core/ConsoleExt/ConsoleMenu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Ajiva.Installer.Core.ConsoleExt
+{
+    public class ConsoleMenu
+    {
+        public void ShowMenu(string title, params ConsoleMenuItem[] items)
+        {
+            Console.WriteLine(title);
+            var formate = new string(Enumerable.Repeat('0', items.Length / 10 + 1).ToArray());
+            var i = 0;
+            foreach (var item in items)
+            {
+                Console.WriteLine($"[{i.ToString(formate)}]: {item.Name}");
+                i++;
+            }
+            int num;
+            do
+            {
+                Console.WriteLine("Chose Action [0...{i}]");
+            } while (int.TryParse(Console.ReadLine(), out num) && num < 0 && num > i);
+
+            items[num].Action?.Invoke();
+        }
+    }
+
+    public record ConsoleMenuItem (string Name, Action? Action);
+}
---
+++ Ajiva.Installer.Core/ConsoleExt/Log.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Ajiva.Installer.Core.ConsoleExt
+{
+    public class LogHelper
+    {
+        public static void Log(object obj)
+        {
+            lock (WriteLock)
+                Console.WriteLine(obj.ToString());
+        }
+
+        public static void WriteLine(object obj) => Log(obj);
+
+        public static void Write(string msg, int position)
+        {
+            lock (WriteLock)
+            {
+                var (left, top) = Console.GetCursorPosition();
+
+                if (top != position) Console.SetCursorPosition(0, position);
+
+                Console.Write(msg);
+
+                if (top != position) Console.SetCursorPosition(left, top);
+            }
+        }
+
+        internal static readonly object WriteLock = new();
+
+        public static string GetInput(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine() ?? "";
+        }
+
+        public static bool YesNow(string s)
+        {
+            while (true)
+            {
+                Log(s + " [y/n]");
+                var key = Console.ReadKey();
+                // ReSharper disable once SwitchStatementHandlesSomeKnownEnumValuesWithDefault
+                switch (key.Key)
+                {
+                    case ConsoleKey.Y:
+                        return true;
+                    case ConsoleKey.N:
+                        return false;
+                    default:
+                        Log("y/n");
+                        break;
+                }
+            }
+        }
+    }
+    public class ConsoleBlock
+    {
+        public readonly int Count;
+        private readonly int top;
+
+        public ConsoleBlock(int count)
+        {
+            lock (LogHelper.WriteLock)
+            {
+                this.Count = count;
+                top = Console.CursorTop;
+                Console.SetCursorPosition(0, top + count);
+            }
+        }
+
+        public void WriteAt(string msg, int position)
+        {
+            if (position > Count) throw new ArgumentOutOfRangeException(nameof(position), position, "expected les than count: " + Count);
+            LogHelper.Write(msg.FillUp(Console.BufferWidth - 1), top + position);
+        }
+    }
+
+    public class ConsoleRolBlock
+    {
+        private ConsoleBlock Block;
+        private int index = 0;
+
+        public ConsoleRolBlock(int count)
+        {
+            Block = new(count);
+        }
+
+        public void WriteNext(string msg)
+        {
+            Block.WriteAt($"[{index:X4}] " + msg, index++ % Block.Count);
+        }
+    }
+}
