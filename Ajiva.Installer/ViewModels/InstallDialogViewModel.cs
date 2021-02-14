f5b8a0c6a37c0a22ad96180931b37c8c0b6d3279
xuri02 <t0bl4s.durst@gmail.com>
2021-02-14 22:56:06 +0100

    added ViewModel's for MainWindow and InstallDialog

---
+++ Ajiva.Installer/ViewModels/InstallDialogViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ReactiveUI;
+
+namespace Ajiva.Installer.ViewModels
+{
+    public class InstallDialogViewModel : ViewModelBase
+    {
+        private bool isPopupVisible = false;
+        private string url;
+        private string key;
+        public bool IsPopupVisible
+        {
+            get => isPopupVisible;
+            set => this.RaiseAndSetIfChanged(ref isPopupVisible, value);
+        }
+        public string Url
+        {
+            get => url;
+            set => this.RaiseAndSetIfChanged(ref url, value);
+        }
+        public string Key
+        {
+            get => key;
+            set => this.RaiseAndSetIfChanged(ref key, value);
+        }
+    }
+}
---
+++ Ajiva.Installer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using ReactiveUI;
+
+namespace Ajiva.Installer.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private bool isPopupVisible = false;
+
+        public ObservableCollection<InstalledInfo> Items { get; } = new(new[]
+        {
+            new InstalledInfo
+            {
+                Description = "Walk the dog", Name = "St", IconSrc = @"./avalonia-logo.ico", ExecutingOptions =
+                {
+                    Args = "/c echo Description && timeout 10",
+                    Executable = "cmd.exe"
+                }
+            },
+            new InstalledInfo {Description = "Buy some milk", Name = "Main"},
+            new InstalledInfo {Description = "Learn Avalonia", Name = "Factorio"},
+        });
+
+        public bool IsPopupVisible
+        {
+            get => isPopupVisible;
+            set => this.RaiseAndSetIfChanged(ref isPopupVisible, value);
+        }
+    }
+}
