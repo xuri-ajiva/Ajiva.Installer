55afa5e2be5dd97670e6300298a326ea7aa02420
xuri02 <t0bl4s.durst@gmail.com>
2021-02-14 22:55:06 +0100

    added BitmapValueConverter

---
+++ Ajiva.Installer/ViewModels/BitmapValueConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using Avalonia.Platform;
+using Bitmap = Avalonia.Media.Imaging.Bitmap;
+
+namespace Ajiva.Installer.ViewModels
+{
+    public class BitmapValueConverter : IValueConverter
+    {
+        public static BitmapValueConverter Instance = new();
+
+        public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // ReSharper disable once ConvertIfStatementToSwitchStatement
+            if (value is string path && (targetType == typeof(IImage)))
+            {
+                var uri = new Uri(path, UriKind.RelativeOrAbsolute);
+                var scheme = uri.IsAbsoluteUri ? uri.Scheme : "file";
+
+                switch (scheme)
+                {
+                    case "file":
+                        return new Bitmap(path);
+                    default:
+                        {
+                            var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
+                            return new Bitmap(assets.Open(uri));
+                        }
+                }
+            }
+            if (value is null)
+            {
+                return SystemIcons.Error.ToBitmap();
+            }
+            throw new NotSupportedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
