73e19edc89c41c3f66edaaecd6498ea32698916e
xuri02 <t0bl4s.durst@gmail.com>
2021-02-18 23:49:29 +0100

    added PackReader and PackBuilder

---
+++ Ajiva.Installer.Core/Installer/Pack/PackBuilder.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace Ajiva.Installer.Core.Installer.Pack
+{
+    internal class PackBuilder : PackBase
+    {
+        private readonly Stream structure;
+        private Stream? data;
+
+        public PackBuilder()
+        {
+            structure = new MemoryStream();
+        }
+
+        public void BuildStructure(StructureDirectory root)
+        {
+            if (structure.Position != 0)
+                throw new ArgumentException("Already Written an structure");
+
+            root.WriteTo(structure);
+        }
+
+        public void BeginType(Stream stream)
+        {
+            if (data != null) throw new InvalidOperationException();
+            data = stream;
+            stream.Write(PackHeader);
+        }
+
+        public long StructureLength => structure.Position;
+
+        public void WriteHeader(PackHead packHead)
+        {
+            if (data == null) throw new InvalidOperationException();
+            data.WriteAsJson(packHead);
+        }
+
+        public void WriteFile(string dirPath, StructureFile file)
+        {
+            if (data == null) throw new InvalidOperationException();
+
+            Span<byte> span = new(Buffer);
+            var fIn = File.OpenRead(Path.Join(dirPath, file.Name));
+
+            long ptr = 0;
+            while (ptr < file.Length)
+            {
+                var read = fIn.Read(span);
+
+                data.Write(read == BufferLength ? span : span.Slice(0, read));
+
+                ptr += read;
+            }
+            data.Flush();
+        }
+
+        public void WriteStructure()
+        {
+            structure.Position = 0; // write from beginning
+            structure.CopyTo(data ?? throw new InvalidOperationException(), BufferLength);
+        }
+    }
+}
---
+++ Ajiva.Installer.Core/Installer/Pack/PackReader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Ajiva.Installer.Core.Installer.Pack
+{
+    internal class PackReader : PackBase
+    {
+        private Stream Data { get; }
+
+        public PackHead? Header { get; private set; }
+
+        public StructureDirectory? Root { get; private set; }
+
+        public PackReader(Stream data)
+        {
+            Data = data;
+        }
+
+        public void ReadHead()
+        {
+            Span<byte> tmp = new byte[PackHeader.Length];
+
+            Data.Read(tmp);
+            if (!tmp.SequenceEqual(PackHeader))
+            {
+                Data.Close();
+                throw new ArgumentException("File is not the right type!");
+            }
+
+            Header = Data.ReadJsonObjectAs<PackHead>(out _);
+        }
+
+        public void ReadStructure()
+        {
+            if (Header == null) throw new InvalidOperationException();
+            if (Root != null) throw new InvalidOperationException();
+            Root = new(StructureSpecialFolder.InstallLocation, Header.Name);
+            Root.ReadFrom(Data);
+        }
+    }
+}
