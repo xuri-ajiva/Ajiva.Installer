262db5d00975845952d1d5cb34a2fbac1c14781f
xuri02 <t0bl4s.durst@gmail.com>
2021-02-18 23:50:09 +0100

    moved head out of file

---
+++ Ajiva.Installer.Core/Installer/Pack/PackHead.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Ajiva.Installer.Core.Installer.Pack
+{
+    public record PackHead(long FilesCount, int HeadLength, string Name, string Description, string Executable, string Arguments)
+    {
+        [JsonConstructor]
+        public PackHead() : this(default!, default!, default!, default!, default!, default!)
+        {
+        }
+    }
+}
