760b6beb336d9ec91424a0bbdb95572871ca1793
xuri02 <t0bl4s.durst@gmail.com>
2021-02-18 23:49:44 +0100

    added writable base

---
+++ Ajiva.Installer.Core/Installer/Pack/IWritable.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+namespace Ajiva.Installer.Core.Installer.Pack
+{
+    internal interface IWritable
+    {
+        void WriteTo(Stream stream);
+        void ReadFrom(Stream stream);
+    }
+}
