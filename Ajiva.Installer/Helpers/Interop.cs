5ca493d9cfaea046685c7bccee83337914430301
xuri02 <t0bl4s.durst@gmail.com>
2021-02-25 16:28:32 +0100

    Adding all DllImport into Interop class

---
+++ Ajiva.Installer/Helpers/Interop.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Ajiva.Installer.ViewModels;
+
+namespace Ajiva.Installer.Helpers
+{
+    internal static class Interop
+    {
+        
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool AttachConsole(uint dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        internal static extern bool FreeConsole();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool FlashWindow(IntPtr hWnd, bool invert);
+
+        [DllImport("kernel32.dll")]
+        internal static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        internal static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        internal const int SW_HIDE = 0;
+        internal const int SW_SHOW = 5;
+
+    }
+}
