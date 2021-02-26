78dbf4bfe1eec1c8bb85e7f337689ee4f3b2f2e5
xuri02 <t0bl4s.durst@gmail.com>
2021-02-26 15:48:54 +0100

    adding AsValue<T> to wrap class's into value types

---
+++ Ajiva.Installer/Models/AsValue.cs
@@ -0,0 +1,21 @@
+﻿namespace Ajiva.Installer.ViewModels
+{
+    public struct AsValue<T>
+    {
+        public T FValue;
+
+        public T Value
+        {
+            get => FValue;
+            set => FValue = value;
+        }
+
+        public AsValue(T t)
+        {
+            FValue = t;
+        }
+
+        public static implicit operator AsValue<T>(T t) => new(t);
+        public static implicit operator T(AsValue<T> t) => t.FValue;
+    }
+}
